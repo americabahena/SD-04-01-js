a536a7a6e7a76f7adb1ed73650ead876346b9105
america bahena <abahena.dev@gmail.com>
2026-05-21 19:07:15 -0600

    Laboratorio arrays terminado

--- src/task6.js
+++ src/task6.js
@@ -2,7 +2,7 @@ const arr1 = ["hello"]
 const arr2 = ["world"]
 
 // Type your code below this line!
-
-
+const nuevoArray = arr1.concat(arr2);
+console.log(nuevoArray);
 
 // Type your code above this line!
